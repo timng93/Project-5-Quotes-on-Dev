69a896948e6f879a29c0d7263efed5978e7989df
Timmy <timgabrielnguyen@gmail.com>
2018-11-28 10:13:18 -0800

    Add Object in JavaScript File

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 (function($) {
   $(function() {
-
     let lastPage = '';
 
+    const $source = $('.source');
 
     $('#new-quote-button').on('click', function(event) {
       event.preventDefault();
@@ -10,8 +10,7 @@
     });
 
     function getQuote() {
-
-        lastPage = document.URL;
+      lastPage = document.URL;
 
       $.ajax({
         method: 'GET',
@@ -19,96 +18,81 @@
           qod_vars.rest_url +
           'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
       })
-              .done(function(data) {
-
+        .done(function(data) {
           history.pushState(null, null, qod_vars.home_url + '/' + data[0].slug);
 
           $('.entry-content').empty();
           $('.entry-title').empty();
-          $('.source').empty();
+          $source.empty();
 
           $('.entry-content').append(` ${data[0].content.rendered}`);
           $('.entry-title').append(`&mdash; ${data[0].title.rendered}`);
 
           if (data[0]._qod_quote_source_url.length > 0) {
-            $('.source').append(`, <a href="${data[0]._qod_quote_source_url}">${data[0]._qod_quote_source}</a>`);
+            $source.append(
+              `, <a href="${data[0]._qod_quote_source_url}">${
+                data[0]._qod_quote_source
+              }</a>`
+            );
           } else if (data[0]._qod_quote_source.length > 0) {
-        
-            $('.source').append(`, ${data[0]._qod_quote_source}`);
-
+            $source.append(`, ${data[0]._qod_quote_source}`);
           } else {
-            $('.source').append("");
-
-
+            $source.append('');
           }
-       
         })
         .fail(function(err) {
-          $('.source').append("");
-
-          
+          $source.append('');
         });
     }
     //end of Get Quote
 
     $(window).on('popstate', function() {
-
       window.location.replace(lastPage);
-
     });
 
-             $('#quote-submission-form').on('submit', function(event){
-                
-            event.preventDefault();
-            postQuote();
+    $('#quote-submission-form').on('submit', function(event) {
+      event.preventDefault();
+      postQuote();
+    });
 
-             });
+    function postQuote() {
+      const quoteTitle = $('#quote-author').val();
 
-             function postQuote() {
+      const quoteContent = $('#quote-content').val();
 
-              const quoteTitle = $('#quote-author').val();
+      const quoteSource = $('#quote-source').val();
 
-               const quoteContent = $('#quote-content').val();
+      const quoteURL = $('#quote-source-url').val();
 
-               const quoteSource = $('#quote-source').val();
+      const JSONValue = {
+        title: quoteTitle,
+        content: quoteContent,
+        _qod_quote_source: quoteSource,
+        _qod_quote_source_url: quoteURL,
+        status: 'publish'
+      };
+      $.ajax({
+        method: 'POST',
+        url: qod_vars.rest_url + 'wp/v2/posts',
+        data: JSONValue,
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', qod_vars.nonce);
+        }
+      })
+        .done(function() {
+          $('#quote-submission-form').slideUp('slow');
+          $('.quote-submission-wrapper').append(
+            '<h3>' + qod_vars.success + '</h3>'
+          );
+        })
+        .fail(function() {
+          $('#quote-submission-form').slideUp('slow');
 
-               const quoteURL = $('#quote-source-url').val();
-              
-              const JSONValue = {
-                'title': quoteTitle,
-                'content': quoteContent,
-                  '_qod_quote_source': quoteSource,
-                  '_qod_quote_source_url': quoteURL,
-                  status: 'publish',
-
-              };
-               $.ajax({
-                 method: 'POST',
-                 url: qod_vars.rest_url + 'wp/v2/posts',
-                 data: JSONValue,
-                 beforeSend: function (xhr) {
-
-                  xhr.setRequestHeader( 'X-WP-Nonce', qod_vars.nonce );
-
-                 }
-
-
-               }).done(function(){
-
-
-                $( "#quote-submission-form" ).slideUp( "slow");
-                $('.quote-submission-wrapper').append('<h3>'+ qod_vars.success + '</h3>');
-
-               }).fail(function(){
-                 
-
-                $( "#quote-submission-form" ).slideUp( "slow");
-
-                $('.quote-submission-wrapper').append('<h3>'+ qod_vars.failure + '</h3>');
-               });
-             }
-
-
+          $('.quote-submission-wrapper').append(
+            '<h3>' + qod_vars.failure + '</h3>'
+          );
+        });
+    }
   });
   //end of doc ready
 })(jQuery);
